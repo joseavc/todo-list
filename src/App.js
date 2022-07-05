4a92b07c10dbaa195587458bfa06762df8e95ea8
joseavc <joseavillalobos1997@gmail.com>
2022-07-05 18:38:24 -0500

    Form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+import React, { useState } from 'react';
 import './App.css';
-
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Jose's Todo List</h1>
+      </header>
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
